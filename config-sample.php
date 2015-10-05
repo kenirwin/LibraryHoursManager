08791f346120654647ffa8d08322ffcd7d16d1e2
Ken Irwin <kirwin@wittenberg.edu>
2015-10-04 20:41:25 -0400

    updated config-sample.php -- variables only, does not initiate connection

--- config-sample.php
+++ config-sample.php
@@ -4,7 +4,4 @@ $database = "";
 $hostname = "";
 $password = "";
 $charset = "utf8";
-
-$db = new PDO("mysql:host=$host;dbname=$db;charset=$charset", "$user", "$pass");
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
