40910249570cd0f10c44adc77fe50ecabf4dab32
Ken Irwin <kirwin@wittenberg.edu>
2015-10-23 14:15:13 -0400

    allow command-line use of generate.php

--- generate.php
+++ generate.php
@@ -1,6 +1,7 @@
 <?
 include ("hours.class.php");
 
+if (is_array($argv)) { Argv2Request($argv); }
 if (isset($_REQUEST['format'])) {
     $format = $_REQUEST['format'];
 }
@@ -32,5 +33,27 @@ default:
 
 if ($format = "xmlIthaca") {
     print '</hours>'.PHP_EOL;
+}
+
+
+function Argv2Request($argv) {
+    /*
+      When $_REQUEST is empty and $argv is defined, 
+      interpret $argv[1]...$argv[n] as key => value pairs
+      and load them into the $_REQUEST array
+
+      This allows the php command line to subsitute for GET/POST values, e.g.
+      php script.php animal=fish color=red number=1 has_car=true has_star=false
+     */
+
+
+    if ($argv !== NULL && sizeof($_REQUEST) == 0) {
+        $argv0 = array_shift($argv); // first arg is different and is not needed
+        
+        foreach ($argv as $pair) {
+            list ($k, $v) = split("=", $pair);
+            $_REQUEST[$k] = $v;
+        }
+    }
 }
 ?>
