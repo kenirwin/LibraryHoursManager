e6eae1a7f3c049f6419b17d4f5c58fd6569cebb5
Ken Irwin <kirwin@wittenberg.edu>
2015-10-23 09:52:06 -0400

    added one-day functions to gen_form

--- gen_form.php
+++ gen_form.php
@@ -18,14 +18,25 @@
 <?
 include ("nav.php");
 ?>
+
+<h2>Get List as XML or Plain Text</h2>
 <form action="generate.php">
-     First Date: <input type="text" name="first_date" class="date-field"><br />
-     Last Date: <input type="text" name="last_date" class="date-field"><br />
-     Format: 
+     <label for="first_date">First Date:</label> <input type="text" name="first_date" class="date-field" /><br />
+     <label for="last_date">Last Date:</label> <input type="text" name="last_date" class="date-field" /><br />
+     <label for="format">Format:</label>
      <select name="format">
      <option value="xmlIthaca">XML</option>
      <option value="text">Text</option>
      </select><br />
-     <input type="hidden" name="action" value="getlist">
-     <input type="submit">
+     <input type="hidden" name="action" value="getlist" />
+     <input type="submit" />
 </form>
+
+     <h2>Get One Day as Plain Text</h2>
+     <p><a href="generate.php?action=oneday" class="button">Today</a> -- suitable for including on another page via curl or php include</p>
+     <form action="generate.php">
+     <input type="hidden" name="action" value="oneday" />
+     <label for="date">Choose date:</label>
+     <input type="text" class="date-field" />
+     <input type="submit" />
+     </form>
