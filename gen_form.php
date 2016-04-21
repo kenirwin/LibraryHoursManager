4cecf619b49fe939d336db190b80237250158dbb
Ken Irwin <kirwin@wittenberg.edu>
2016-04-21 15:34:38 -0400

    switched to locally hosted jquery

--- gen_form.php
+++ gen_form.php
@@ -1,13 +1,12 @@
 <html>
 <head>
 <meta name=viewport content="width=device-width, initial-scale=1">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="./lib/scripts/jquery-2.2.3.min.js"></script>
 <link rel="stylesheet" href="style.css" />
-<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
 
     <link href="./lib/themes/redmond/jquery-ui-1.8.16.custom.css" rel="stylesheet" type="text/css" />
 
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+<script src="./lib/scripts/jquery-ui.1.11.4.min.js"></script>
 <script type="text/javascript">
      $(function() {
          $('#nav-buttons li').button();
